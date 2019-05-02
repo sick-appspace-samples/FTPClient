14415a78caa10a0f84f5c324aada47e755d7d2b7
Florian Roser <Florian.Roser@sick.de>
2019-05-02 17:04:27 +0200

    Initial commit

---
+++ FTPClient/scripts/FTPClient.lua
@@ -0,0 +1,67 @@
+--[[----------------------------------------------------------------------------
+
+  Application Name: FTPClient
+
+  Description:
+  Creating FTP Client
+
+
+  This sample sets up a FTP server listening on port 12345 with a user account
+  serving the contents of the ram filesystem. Then it creates an FTP client,
+  connects to the server and uploads a file.
+
+  The script can be run using the emulator. If everything works, the script reports
+  that the file has been successfully written.
+
+------------------------------------------------------------------------------]]
+--Start of Global Scope---------------------------------------------------------
+
+-- Create a ftp server instance
+local ftpServer = FTPServer.create()
+-- Set the server port to 12345
+FTPServer.setServerPort(ftpServer, 12345)
+-- Create a user account with home directory pointing to ram filesystem
+FTPServer.addUser(ftpServer, 'user', 'password', 'private')
+-- Start the ftp server
+FTPServer.start(ftpServer)
+
+-- Create a ftp client instance
+local handle = FTPClient.create()
+-- Set server ip address and port
+FTPClient.setIpAddress(handle, '127.0.0.1')
+FTPClient.setPort(handle, 12345)
+
+-- Try to connect to the server
+if (FTPClient.connect(handle, 'user', 'password')) then
+  -- Set transfer mode to ascii
+  if (FTPClient.setMode(handle, 'ASCII')) then
+    -- Remove test directory (might have been created in a preceding run)
+    FTPClient.rmdir(handle, 'test')
+    -- Create test directory
+    FTPClient.mkdir(handle, 'test')
+    -- Change to test directory
+    if (FTPClient.cd(handle, 'test')) then
+      -- Put a file with dummy contents
+      if (FTPClient.put(handle, 'file.txt', 'dummy file contents')) then
+        print('File successfully written to FTP server')
+      else
+        print('Failed to write file')
+      end
+    else
+      print('Failed to change directory')
+    end
+  else
+    print('Failed to set mode')
+  end
+else
+  print('Failed to connect to FTP server')
+end
+
+-- Disconnect the client
+FTPClient.disconnect(handle)
+-- Stop the ftp server
+FTPServer.stop(ftpServer)
+
+print('App finished.')
+
+--End of Global Scope-----------------------------------------------------------
